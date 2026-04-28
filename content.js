25ba8fe7081ecacee47749b7131f95f4de89a065
João Vitor Nogueira <jvnogueira2010@gmail.com>
2026-04-28 14:25:53 -0300

    Inclui campo de número para determinar onde o script vai começar

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
 document.getElementById("run").addEventListener("click", async () => {
 
+    let valorInput = document.getElementById("startIndex").value;
+
+    let indiceInicial = valorInput === "" ? 0 : parseInt(valorInput);
+
     let [tab] = await chrome.tabs.query({
         active: true,
         currentWindow: true
@@ -7,13 +11,29 @@ document.getElementById("run").addEventListener("click", async () => {
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: () => {
+        args: [indiceInicial],
+        func: (indiceInicial) => {
 
             // Referencia cada resposta em alguma públicação (alterar o primeiro elemento para 0 ou 1)
             element = document.querySelectorAll("[aria-label='Corpo da coluna']")[0].children[0].children[3].children[0].children
 
-            i = 0;
-            start()
+            i = indiceInicial;
+
+            numberBox()
+
+            function numberBox(){
+                if(i < element.length){
+                    start()
+                    console.log("0.0")
+                }else{
+                    console.log("0.1")
+                    setTimeout(() => {
+                        element[element.length-2].scrollIntoView()
+                        numberBox();
+                    }, 3000);
+                }
+            }
+
             function start(){
 
                 highlight();
@@ -36,12 +56,13 @@ document.getElementById("run").addEventListener("click", async () => {
                 }else{
                     console.log("*id: "+i+" Like: "+likeButton.innerText+" Repost: "+repostButton.innerText+" Share: "+shareButton.innerText)
                     if(i < element.length-3){
+                        console.log("1.0")
                         setTimeout(() => {
                             i++
                             start()
                         }, 500);
                     }else{
-                        console.log("*id: "+i+" Like: "+likeButton.innerText+" Repost: "+repostButton.innerText+" Share: "+shareButton.innerText)
+                        console.log("1.1")
                         setTimeout(() => {
                             i++
                             start()
@@ -52,16 +73,36 @@ document.getElementById("run").addEventListener("click", async () => {
 
             function deleteCommit(element2){
                 setTimeout(() => {
+                    console.log("2.0")
                     // Botão três pontinhos
                     configButton = element2.querySelectorAll("[aria-label='Mais']")[0].parentElement;
 
                     configButton.click()
                     setTimeout(() => {
+                        console.log("2.1")
                         // Botão de Excluir 
-                        excludeOption = [...document.querySelector("[role='menu']").querySelectorAll("span")].find(el => el.innerText.includes("Excluir"));
+                        const menu = document.querySelector("[role='menu']");
+
+                        if (!menu) {
+                            console.log("menu não encontrado");
+
+                            setTimeout(() => {
+                                i++;
+                                start();
+                            }, 3000);
+
+                            return;
+                        }
+
+                        const excludeOption =
+                        [...menu.querySelectorAll("span")]
+                        .find(el => el.innerText.includes("Excluir"));
+                        
                         if(excludeOption){
+                            console.log("3.0")
                             excludeOption.click()
                         }else{
+                            console.log("3.1")
                             setTimeout(() => {
                                 i++
                                 start();
